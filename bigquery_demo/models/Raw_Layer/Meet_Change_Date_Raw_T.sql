c7dc98cc931e14ecb8fce95c0e29c4278d41e288
nirajbais1 <niraj.bais007@gmail.com>
2024-06-21 17:51:29 +0100

    Updating the models with different layering structure

--- bigquery_demo/models/Raw_Layer/Meet_Change_Date_Raw_T.sql
+++ bigquery_demo/models/Raw_Layer/Meet_Change_Date_Raw_T.sql
@@ -1,4 +1,4 @@
 SELECT meetid,FORMAT_DATE("%d/%m/%Y", DATE ) as UK_date, MeetTown 
 FROM  
 
-{{ source("data_ingestion","meets") }}  where meetid <5
+{{ source("data_ingestion","meets") }}  where meetid <5 
